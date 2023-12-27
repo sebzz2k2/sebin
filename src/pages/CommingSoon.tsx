10fc8db8eba6a964517429a0b3fd5a1baa21deb5
Sebin CM <sebinsebzz2002@gmail.com>
2023-12-28 00:07:35 +0530

    make responsive

--- src/pages/CommingSoon.tsx
+++ src/pages/CommingSoon.tsx
@@ -2,7 +2,7 @@ const CommingSoon = () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-black p-10">
       <div className="w-max">
-        <h1 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-5xl  font-bold font-blackOpsOne text-[#27DBBC]">
+        <h1 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 sm:text-xl lg:text-5xl  font-bold font-blackOpsOne text-[#27DBBC]">
           I am creating something new..
         </h1>
       </div>
