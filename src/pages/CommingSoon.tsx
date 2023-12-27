80e4f8666e85ffd846581f00c0a1facafe330bb0
Sebin CM <sebinsebzz2002@gmail.com>
2023-12-27 23:19:30 +0530

    fix build issues

--- src/pages/CommingSoon.tsx
+++ src/pages/CommingSoon.tsx
@@ -1,11 +1,9 @@
-import React from "react";
-
 const CommingSoon = () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-black p-10">
       <div className="w-max">
         <h1 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-5xl  font-bold font-blackOpsOne text-[#27DBBC]">
-          I am creating something
+          I am creating something new..
         </h1>
       </div>
     </div>
