377b7420896c014964f4ad93ea8fabd8260da500
Sebin CM <sebinsebzz2002@gmail.com>
2024-01-15 17:46:17 +0530

    add typing animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,28 @@ const config: Config = {
       fontFamily: {
         firaCode: ["Fira Code", "monospace"],
       },
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden",
+          },
+          "100%": {
+            width: "100%",
+          },
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent",
+          },
+          "100%": {
+            borderColor: "cyan-600",
+          },
+        },
+      },
+      animation: {
+        typing: "typing 4s steps(20) infinite alternate, blink .7s infinite",
+      },
     },
   },
   plugins: [],
