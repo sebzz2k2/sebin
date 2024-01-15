7dc73fd30e6b0c9be0f70f22f2e41c62534c34f5
Sebin CM <sebinsebzz2002@gmail.com>
2024-01-15 18:18:54 +0530

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { GrLinkNext } from "react-icons/gr";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +28,7 @@ export default function RootLayout({
             <div className="shape-blob three"></div>
           </div>
           {children}
+          <Analytics />
         </div>
       </body>
     </html>
