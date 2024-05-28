c3e01bbdb609a000c7ad3b3c838b455d0c9b2734
Sebin CM <sebinsebzz2002@gmail.com>
2024-05-28 09:37:43 +0530

    feat: add react time-line component with temp data

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,7 +1,64 @@
+"use client";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 import React from "react";
+import { GrForwardTen } from "react-icons/gr";
 
 const Experience = () => {
-  return <div className="h-screen">Experience</div>;
+  return (
+    <div className="h-screen">
+      <ExperienceTimeline />
+    </div>
+  );
 };
 
 export default Experience;
+
+const ExperienceTimeline = () => {
+  return (
+    <VerticalTimeline>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        date="April 2013"
+        icon={<GrForwardTen />}
+        visible={true}
+        iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+      >
+        <h3 className="vertical-timeline-element-title">
+          Content Marketing for Web, Mobile and Social Media
+        </h3>
+        <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+        <p>Strategy, Social Media</p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        date="April 2013"
+        icon={<GrForwardTen />}
+        visible={true}
+        iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+      >
+        <h3 className="vertical-timeline-element-title">
+          Content Marketing for Web, Mobile and Social Media
+        </h3>
+        <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+        <p>Strategy, Social Media</p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        date="April 2013"
+        icon={<GrForwardTen />}
+        visible={true}
+        iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+      >
+        <h3 className="vertical-timeline-element-title">
+          Content Marketing for Web, Mobile and Social Media
+        </h3>
+        <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+        <p>Strategy, Social Media</p>
+      </VerticalTimelineElement>
+    </VerticalTimeline>
+  );
+};
