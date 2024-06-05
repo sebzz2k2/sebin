3bc95d95fe4d0bd7f3808c23df01e7091a6a254e
Sebin CM <sebinsebzz2002@gmail.com>
2024-06-05 12:16:25 +0530

    feat: experience

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -6,10 +6,12 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 import React from "react";
 import { GrForwardTen } from "react-icons/gr";
+import { TbTarget } from "react-icons/tb";
+import axios from "axios";
 
 const Experience = () => {
   return (
-    <div className="h-screen">
+    <div className="h-[calc(100vh-6rem)] px-16 py-8 flex  flex-col gap-16 overflow-auto">
       <ExperienceTimeline />
     </div>
   );
@@ -18,47 +20,46 @@ const Experience = () => {
 export default Experience;
 
 const ExperienceTimeline = () => {
+  const [experiences, setExperiences] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchExperiences = async () => {
+      const experiencesUrl =
+        "https://raw.githubusercontent.com/sebzz2k2/sebin-assets/main/portfolio/experience.json";
+      const response = await axios.get(experiencesUrl);
+      setExperiences(response.data);
+    };
+    fetchExperiences();
+  }, []);
   return (
     <VerticalTimeline>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--education"
-        date="April 2013"
-        icon={<GrForwardTen />}
-        visible={true}
-        iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-      >
-        <h3 className="vertical-timeline-element-title">
-          Content Marketing for Web, Mobile and Social Media
-        </h3>
-        <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-        <p>Strategy, Social Media</p>
-      </VerticalTimelineElement>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--education"
-        date="April 2013"
-        icon={<GrForwardTen />}
-        visible={true}
-        iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-      >
-        <h3 className="vertical-timeline-element-title">
-          Content Marketing for Web, Mobile and Social Media
-        </h3>
-        <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-        <p>Strategy, Social Media</p>
-      </VerticalTimelineElement>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--education"
-        date="April 2013"
-        icon={<GrForwardTen />}
-        visible={true}
-        iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-      >
-        <h3 className="vertical-timeline-element-title">
-          Content Marketing for Web, Mobile and Social Media
-        </h3>
-        <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-        <p>Strategy, Social Media</p>
-      </VerticalTimelineElement>
+      {experiences.map((experience: any) => (
+        <VerticalTimelineElement
+          key={experience.id}
+          date={`${experience.startDate} - ${experience.endDate}`}
+          dateClassName="text-2xl font-firaCode font-bold text-cyan-900"
+          iconStyle={{ background: "#2d3748", color: "#fff" }}
+          icon={<TbTarget />}
+          visible={true}
+        >
+          <h3 className="vertical-timeline-element-title text-xl font-firaCode font-bold text-cyan-900">
+            {experience.title}
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle font-md font-firaCode font-bold text-cyan-800 ">
+            {experience.company}
+          </h4>
+          <ul className="list-disc list-inside">
+            {experience.responsibilities.map((desc: string) => (
+              <li
+                key={desc}
+                className="text-sm font-firaCode text-cyan-800 mt-1"
+              >
+                {desc}
+              </li>
+            ))}
+          </ul>
+        </VerticalTimelineElement>
+      ))}
     </VerticalTimeline>
   );
 };
