927c185334f3bfbdc08212db5011339b8dc117ab
sebin <sebinsebzz2002@gmail.com>
2024-09-21 00:31:50 +0530

    fix build

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -6,38 +6,4 @@ type IExperience = {
   responsibilities: string[];
 };
 
-type CoverImage = {
-  url: string;
-};
-
-export type GetPostBySlugResponse = {
-  publication: {
-    post: {
-      title: string;
-      subtitle?: string;
-      coverImage: CoverImage | null;
-      content: {
-        html: string;
-      };
-      author: {
-        name: string;
-        profilePicture?: string;
-      };
-    };
-  };
-};
-
-type BlogPost = {
-  node: {
-    id: string;
-    title: string;
-    brief: string;
-    publishedAt: string;
-    slug: string;
-    views: number;
-    readTimeInMinutes: number;
-    coverImage: CoverImage | null;
-  };
-  cursor: string;
-};
-export type { IExperience, BlogPost };
+export type { IExperience };
