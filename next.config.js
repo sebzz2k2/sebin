7f7d1c2683184dc27016ee41ddbb5f1dbea1a11c
sebin <sebinsebzz2002@gmail.com>
2024-09-20 21:12:25 +0530

    chore: use hashnode template

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	async rewrites() {
+		return [
+			{
+				source: "/blog",
+				destination: "https://starter-kit-mu.vercel.app/blog",
+			},
+			{
+				source: "/blog/:path*",
+				destination: "https://starter-kit-mu.vercel.app/blog/:path*",
+			},
+		];
+	},
+}
 
 module.exports = nextConfig
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,72 +1,71 @@
 "use client";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import {usePathname} from "next/navigation";
 import "./styles.css";
 import Button from "./common/button";
 
 const Navbar = () => {
-  return (
-    <nav className="fixed w-full z-50">
-      <div className="flex justify-between items-center py-6 px-24">
-        <div className="flex items-center"></div>
-        <div className="flex items-center w-2/5 justify-between">
-          <NavLinks />
-          <Button width="120px" height="40px">
-            <Link
-              href="https://github.com/sebzz2k2/sebin-assets/raw/main/sebin-resume.pdf"
-              className="flex items-center justify-center w-[120px] h-[40px]"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              Resume
-            </Link>
-          </Button>
-        </div>
-      </div>
-    </nav>
-  );
+	return (
+		<nav className="fixed w-full z-50">
+			<div className="flex justify-between items-center py-6 px-24">
+				<div className="flex items-center"></div>
+				<div className="flex items-center w-2/5 justify-between">
+					<NavLinks />
+					<Button width="120px" height="40px">
+						<Link
+							href="https://github.com/sebzz2k2/sebin-assets/raw/main/sebin-resume.pdf"
+							className="flex items-center justify-center w-[120px] h-[40px]"
+							rel="noopener noreferrer"
+							target="_blank"
+						>
+							Resume
+						</Link>
+					</Button>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
 
 const navContents = [
-  {
-    name: "Home",
-    href: "/",
-  },
-  {
-    name: "Projects",
-    href: "/projects",
-  },
-  {
-    name: "Experience",
-    href: "/experience",
-  },
-  {
-    name: "Blogs",
-    href: "/blogs",
-  },
+	{
+		name: "Home",
+		href: "/",
+	},
+	{
+		name: "Projects",
+		href: "/projects",
+	},
+	{
+		name: "Experience",
+		href: "/experience",
+	},
+	{
+		name: "Blog",
+		href: "/blog",
+	},
 ];
 
 const NavLinks = () => {
-  const pathname = usePathname();
-  return (
-    <>
-      {navContents.map((navItem, idx) => (
-        <Link
-          key={idx}
-          className="inline-block text-lg text-cyan-950 no-underline hover:text-gray-800 hover:text-underline py-2 px-4 "
-          href={navItem.href}
-        >
-          <span
-            className={`font-firaCode link link-underline link-underline-black pb-[2px] ${
-              pathname === navItem.href ? "link-active" : ""
-            }`}
-          >
-            {navItem.name}
-          </span>
-        </Link>
-      ))}
-    </>
-  );
+	const pathname = usePathname();
+	return (
+		<>
+			{navContents.map((navItem, idx) => (
+				<Link
+					key={idx}
+					className="inline-block text-lg text-cyan-950 no-underline hover:text-gray-800 hover:text-underline py-2 px-4 "
+					href={navItem.href}
+				>
+					<span
+						className={`font-firaCode link link-underline link-underline-black pb-[2px] ${pathname === navItem.href ? "link-active" : ""
+							}`}
+					>
+						{navItem.name}
+					</span>
+				</Link>
+			))}
+		</>
+	);
 };
