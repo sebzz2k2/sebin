941ad72dbd348b4acf35c627008b56dc4c93b88d
sebin <sebinsebzz2002@gmail.com>
2024-09-24 21:56:54 +0530

    add few redirects

--- next.config.js
+++ next.config.js
@@ -10,6 +10,18 @@ const nextConfig = {
 				source: "/blog/:path*",
 				destination: "https://starter-kit-mu.vercel.app/blog/:path*",
 			},
+			{
+				source: "/gh",
+				destination: "https://github.com/sebzz2k2"
+			},
+			{
+				source: "/x",
+				destination: "https://x.com/sebin2k2"
+			},
+			{
+				source: "/in",
+				destination: "https://www.linkedin.com/in/sebin-chacko-mathew-29404722a/"
+			}
 		];
 	},
 }
